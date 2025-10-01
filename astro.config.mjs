c77a66e7eb9d9f110f54f0b73e173c3f76704874
Mike Hardin <mhardinla@gmail.com>
2025-10-01 11:42:51 -0700

    added astro-mixpanel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import mixpanel from "astrojs-mixpanel";
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  integrations: [
+    mixpanel({
+      token: "1ef9c686254ddbfa5de83e3d5f07359a",
+      config: {
+        track_pageview: true,
+        persistence: "localStorage",
+        batch_requests: true,
+        debug: true,
+      }
+    }),
+  ],
+  
 });
